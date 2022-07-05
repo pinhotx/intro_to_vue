f03cb4838deafa7fb0b684cf8ec443cc69bd4dd9
Pinho Santiago~ <tiago.apsilveira@outlook.com>
2022-07-05 04:11:54 -0300

    L3-end

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'A warm fuzzy pair of socks.' 
+            description: 'A warm fuzzy pair of socks.',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.twitch.tv/pinhotx'
           
         }
     }
